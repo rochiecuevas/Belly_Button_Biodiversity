cf03c91b5f6d3088d7016e2a67ac120b5c5203e5
Rochie Cuevas <rochiecuevas@gmail.com>
2018-12-26 08:28:33 -0800

    added code to get data from sample-based metadata

--- static/js/app.js
+++ static/js/app.js
@@ -21,6 +21,7 @@ d3.json(urlMeta).then(function(trace){
 
     // Create a list of sample IDs that the reader can choose from in the select field
     var optionsList = data[0]["sample"]; 
+    optionsList.unshift("All");
     console.log(optionsList);
 
     // Populate the select field with each option from the option list
@@ -31,37 +32,39 @@ d3.json(urlMeta).then(function(trace){
         .text(function(sample){
             return sample;
         })
-     
+        
     // Fill in the table with selected values
     // (1) Selection of a sample
     function handleChange(){
         var selection = sampleID.property("value"); // numbers
         console.log(selection); // number has been converted to string
 
-        // (2) Filtering the table based on the values from the selection
-        // Since the JSON object is a dictionary of lists, the index of the selected value must be determined
+        // (2) Change the url for the metadata (include sample)
+        var urlMeta1 = `/metadata/${selection}`;
+        console.log(urlMeta1)
 
-        var selIndex = optionsList.indexOf(Number(selection)) // converts the string to a number
-        console.log(selIndex)
+        // (3) Use the new url to populate the HTML table
+        d3.json(urlMeta1).then(function(trace){
+            var data = [trace];
 
-        // (3) Get the values that match the index of the selected item in the array
-        var tdAge = d3.select("#age");
-        var tdGender = d3.select("#gender");
-        var tdEvent = d3.select("#sampling-event");
-        var tdBBType = d3.select("#bbtype");
-        var tdLocation = d3.select("#location");
-        var tdID = d3.select("#id");
+            // Get the values that match the index of the selected item in the array
+            var tdAge = d3.select("#age");
+            var tdGender = d3.select("#gender");
+            var tdEvent = d3.select("#sampling-event");
+            var tdBBType = d3.select("#bbtype");
+            var tdLocation = d3.select("#location");
+            var tdID = d3.select("#id");
 
-        tdAge.text(data[0]["age"][selIndex]);
-        tdGender.text(data[0]["gender"][selIndex]);
-        tdEvent.text(data[0]["sampling_event"][selIndex]);
-        tdBBType.text(data[0]["bbtype"][selIndex]);
-        tdLocation.text(data[0]["location"][selIndex]);
-        tdID.text(data[0]["sample"][selIndex]);
+            var tdList = [tdAge, tdBBType, tdEvent, tdGender, tdID, tdLocation];
+            var catList = ["age", "bbtype", "sampling_event", "gender", "sample", "location"];
 
-        };
-        sampleID.on("change", handleChange);
+            // Populate the HTML table with the values from the data array
+            for (var i = 0; i < tdList.length; i ++){
+                tdList[i].text(data[0][catList[i]])
+            };
+            });
 
-    
+    };
+    sampleID.on("change", handleChange);
 
 });
